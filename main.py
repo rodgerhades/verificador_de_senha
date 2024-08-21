a172d99951bad6df1f3690c966d2444a02026d0a
Rodger Cabrerisso <83619204+rodgerhades@users.noreply.github.com>
2024-08-21 19:56:56 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#condição
+
+senha = int(input("escolha uma senha: "))
+
+verific = int(input("qual a senha? "))
+if senha == verific :
+    print("senha correta")
+else:
+    print("senha incorreta")
